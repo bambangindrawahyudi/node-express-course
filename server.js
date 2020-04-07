c3bac1b8ef1b68f80a9d53ed40c8280c0ee14689
bambangindrawahyudi <54854928+bambangindrawahyudi@users.noreply.github.com>
2020-04-08 06:56:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const  express  =  membutuhkan ( ' express ' );
 app const  = express ();  
+const  mockUserData = [
+{name : ' Mark ' },
+{name : ' Jill ' }
+]
+aplikasi . get ( ' / users ' , function ( req , res ) {
+ 	 res . json ({
+ 	 	sukses :  benar ,
+ 	 	pesan :  ' berhasil mendapatkan pengguna. Bagus! ' ,
+ 	 	pengguna : mockUserData
+ 	})
+})
