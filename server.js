43b235a9c02206a48b922bdfefb249d9a3afe8ea
bambangindrawahyudi <54854928+bambangindrawahyudi@users.noreply.github.com>
2020-04-08 07:10:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -22,5 +22,40 @@ app.get('/users/:id',function(req,res){
 		user: req.params.id
 	})
 })
+// titik dua digunakan sebagai variabel yang dilihat di 
+aplikasi params . get ( ' / users /: id ' , function ( req , res ) {
+	 console . log ( req . params . id )
+	 res . json ({
+		sukses :  benar ,
+		pesan :  ' dapatkan satu pengguna ' ,
+		pengguna :  req . params . Indo
+	})
+})
+
+aplikasi . post ( ' / login ' , function ( req , res ) {
+	 // Biasanya kata sandi dienkripsi menggunakan sesuatu seperti bcrypt sebelum mengirim ke database 
+	const  username = req . body . username ;
+	 const  password = req . body . password ;
+
+	// Ini harus berasal dari database 
+	const  mockUsername = " billyTheKid " ;
+	const  mockPassword = " superSecret " ;
+
+	if (username === mockUsername && password === mockPassword) {
+		 // Dalam praktiknya, gunakan metode tanda token web JSON di sini untuk membuat 
+		res token terenkripsi . json ({
+			sukses :  benar ,
+			message :  ' kata sandi dan nama pengguna cocok! ' ,
+			token :  ' token terenkripsi ada di sini '
+		})
+	} else {
+		 res . json ({
+			sukses :  salah ,
+			pesan :  ' kata sandi dan nama pengguna tidak cocok '
+		})
+	}
+
+})
 
 app.listen(8000,function(){console.log('server is listening')})
+
